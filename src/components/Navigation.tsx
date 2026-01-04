16d3deda812f48b38e6fd99c40db9722379aed21
nina-uchtmann <ninaucht@uw.edu>
2026-01-04 15:11:14 -0800

    Update page link in nav bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,14 @@
-//import Link from 'next/link';
+import Link from 'next/link';
 
 export default function Navigation() {
   return (
     <nav className="max-6xl mx-auto p-10 flex justify-between">
         <span className="font-display text-4xl tracking-wider">NINA UCHTMANN</span>
         <ul className="flex gap-30 tracking-wider text-2xl">
-            <li><a href="#about">Home</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">About Me</a></li>
-          </ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/projects">Projects</Link></li>
+          <li><Link href="/about">About Me</Link></li>
+        </ul>
     </nav>
   );
 }
