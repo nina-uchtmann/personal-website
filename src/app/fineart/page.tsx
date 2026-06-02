4f3e3dc3579c0fad7e21197053b60742206bdbab
nina-uchtmann <ninaucht@uw.edu>
2026-06-02 12:24:05 -0700

    Fine Arts page formating and ui updates

--- src/app/fineart/page.tsx
+++ src/app/fineart/page.tsx
@@ -5,14 +5,14 @@ const FineArt = () => {
     
 
     return (
-            <section className="flex flex-col pr-48 pl-48 pt-40 pb-40">
-                <div className="flex flex-col gap-5 max-w-l">
+            <section className="flex flex-col pr-48 pl-60 pt-20 pb-40">
+                <div className="flex flex-col gap-5 max-w-lg">
                     {artworks.map((art, index) => (
-                        <div key={index} className="flex items-center gap-8 w-full">
+                        <div key={index} className="flex items-center gap-10 w-full">
                             <img
                                 src={art.src}
                                 alt={art.caption}
-                                className="w-full max-w-md rounded-lg shadow-md"
+                                className="rounded-lg bg-bluegray p-3 shadow-[10px_10px_10px_0px_rgba(0,0,0,0.25)]"
                             />
                             <p className="text-gray-600 whitespace-nowrap">
                                 {art.caption}
--- src/data/finearts.ts
+++ src/data/finearts.ts
@@ -4,12 +4,12 @@ export type Artwork = {
 };
 
 export const artworks: Artwork[] = [
-    { src: '/images/cp_white_rose.jpeg', caption: 'Color Pencil | White Rose' },
-    { src: '/images/wc_pink_rose.jpg', caption: 'Watercolor | Pink Rose' },
-    { src: '/images/cp_magnolia.jpeg', caption: 'Color Pencil | Magnolia' },
-    { src: '/images/cp_purple_lily.jpeg', caption: 'Color Pencil | Lily' },
-    { src: '/images/wc_red_rose.jpeg', caption: 'Watercolor | Red Rose' },
-    { src: '/images/wc_pink_lotus_1.jpeg', caption: 'Watercolor | Lotus' },
-    { src: '/images/wc_pink_lotus.jpeg', caption: 'Watercolor | Lotus' },
-    { src:'/images/wc_redyellow_tulip.jpeg', caption: 'Watercolor | Tulip' },
+    { src: '/images/cp_white_rose.jpeg', caption: 'Color Pencil | 2025 | White Rose' },
+    { src: '/images/wc_pink_rose.jpg', caption: 'Watercolor | 2025 | Pink Rose' },
+    { src: '/images/cp_magnolia.jpeg', caption: 'Color Pencil | 2025 | Magnolia' },
+    { src: '/images/cp_purple_lily.jpeg', caption: 'Color Pencil | 2025 | Lily' },
+    { src: '/images/wc_red_rose.jpeg', caption: 'Watercolor | 2024 | Red Rose' },
+    { src: '/images/wc_pink_lotus_1.jpeg', caption: 'Watercolor | 2024 | Lotus' },
+    { src: '/images/wc_pink_lotus.jpeg', caption: 'Watercolor | 2024 | Lotus' },
+    { src:'/images/wc_redyellow_tulip.jpeg', caption: 'Watercolor | 2024 | Tulip' },
 ];
